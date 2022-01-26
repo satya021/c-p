f69a613501c15d0e152aba0cf4baff99011a8026
satyabrata <92144172+satya021@users.noreply.github.com>
2022-01-26 22:59:14 +0530

    Update user.c

--- user.c
+++ user.c
@@ -4,4 +4,15 @@ int main()
   int a,b,c;
   printf("enter the a");
   scanf("%d",&a);
+  printf("enter the b");
+
+  scanf("%d",&b);
+
+  printf("enter the c");
+
+  scanf("%d",&c);
   
+  printf("sum of a and b is %d \n",a+b);
+  return 0;
+
+ 
